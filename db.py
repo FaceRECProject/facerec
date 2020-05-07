ee4bfe6bbfadd7618200fa56d13135b135378885
MeAquaaaa <62415162+MeAquaaaa@users.noreply.github.com>
2020-05-07 14:48:19 +0800

    DB operations
    FIX some issues

--- db.py
+++ db.py
@@ -35,6 +35,15 @@ def fetch_info(id):
     db.close()
     return val
 
+def fetch_id():
+    db = mysql.connector.connect(host='localhost', user='user', passwd='user123', db='fs')
+    cursor = db.cursor()
+    sql = "SELECT ID FROM user_info "
+    cursor.execute(sql)
+    val = cursor.fetchall()
+    return val
+    db.close()
+
 def save_feature(id,feature):
     db = mysql.connector.connect(host='localhost', user='user', passwd='user123', db='fs')
     cursor = db.cursor()
@@ -63,16 +72,18 @@ def get_features():
         return 'error'
     db.close()
 
-def save_att(no,id,datetime,date):
+def save_att(no,id,datetime,date,abs='0',latetime='0',overtime='0'):
     db = mysql.connector.connect(host='localhost', user='user', passwd='user123', db='fs')
+    print(db)
     cursor = db.cursor()
-    sql_get = "SELECT * FROM attendance WHERE no=%s AND id = %s"
-    NO = (no,id)
+    sql_get = "SELECT * FROM attendance WHERE date=%s AND id = %s"
+    NO = (date,id)
     cursor.execute(sql_get, NO)
     val = cursor.fetchall()
-    if val == NULL:
-        sql = "INSERT INTO attendance(no,id,start,end,date,count)VALUES(%s,%s,%s,%s,%d)"
-        time = (no,id,datetime,datetime,date,1)
+    # print(val)
+    if val == []:
+        sql = "INSERT INTO attendance(no,id,start,end,date,latetime,overtime,abs)VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+        time = (no,id,datetime,datetime,date,latetime,overtime,abs)
         try:
             cursor.execute(sql, time)
             db.commit()
@@ -81,17 +92,17 @@ def save_att(no,id,datetime,date):
             db.rollback()
             return 'error'
     else:
-        sql = "UPDATE attendance SET end = %s,count=%d WHERE no=%s AND id = %s"
-        time = (datetime,2,no,id)
+        sql = "UPDATE attendance SET end = %s,overtime=%s WHERE date=%s AND id = %s"
+        time = (datetime,overtime,date,id)
         cursor.execute(sql, time)
         db.commit()
     db.close()
 
-def get_count(no):
+def get_his(id,date):
     db = mysql.connector.connect(host='localhost', user='user', passwd='user123', db='fs')
     cursor = db.cursor()
-    sql = "SELECT count FROM attendance WHERE no = %s"
-    NO = (no,)
+    sql = "SELECT * FROM attendance WHERE date=%s and id=%s"
+    NO = (date,id)
     try:
         cursor.execute(sql,NO)
         val = cursor.fetchall()
@@ -109,22 +120,22 @@ def statistics(id,no,mon=0,late=0,absence=0):
     NO = (no,id)
     cursor.execute(sql, NO)
     val = cursor.fetchall()
-    if val == NULL:
-        insert = "INSERT INTO statistics(s_no,s_id,mon,late,absence)VALUES(%s,%s,%d,%d,%d)"
+    if val == []:
+        insert = "INSERT INTO statistics(s_no,s_id,mon,late,absence)VALUES(%s,%s,%s,%s,%s)"
         data = (no,id,mon,late,absence)
         cursor.execute(insert,data)
         db.commit()
     else:
-        update = "UPDATE statistics SET mon = %d,late=%d , absence = %d WHERE s_no=%s AND s_id = %s"
+        update = "UPDATE statistics SET mon = %s,late=%s , absence = %s WHERE s_no=%s AND s_id = %s"
         data = (mon,late,absence,no,id)
         cursor.execute(insert, data)
         db.commit()
 
-def get_statistics(no,id):
+def get_statistics(month,id):
     db = mysql.connector.connect(host='localhost', user='user', passwd='user123', db='fs')
     cursor = db.cursor()
-    sql = "SELECT mon,late,absence FROM statistics WHERE s_no = %s AND s_id = %s"
-    no = (no,id)
+    sql = "SELECT late,absence FROM statistics WHERE mon = %s AND s_id = %s"
+    no = (month,id)
     try:
         cursor.execute(sql, no)
         val = cursor.fetchall()
@@ -133,22 +144,3 @@ def get_statistics(no,id):
         db.rollback()
         return 'error'
     db.close()
-# f = np.array([1,2,3])
-# f= f.tolist()
-# # print(f)
-# save_feature('0221',f)
-# features = get_features()
-# id = []
-# feature = []
-# if features != 'error':
-#     for val in features:
-#         id.append(val[0])
-#         c = [int(y) for y in (val[1].split(','))]
-#         feature.append(c)
-# print(id,feature)
-
-# f = [1,2,3,3]
-# x = ','.join([str(i)for i in f])
-# print(type(x))
-# c = [int(y) for y in (x.split(','))]
-# print(type(c),c)
